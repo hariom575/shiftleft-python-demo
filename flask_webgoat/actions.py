8aae9d5f6e04a8569bd2950a276734029ae6e68b
hariom575 <mewadahariom520@gmail.com>
2026-03-26 21:04:29 +0530

    Vulnerability fixed using github copilot

--- flask_webgoat/actions.py
+++ flask_webgoat/actions.py
@@ -37,12 +37,10 @@ def log_entry():
     return jsonify({"success": True})
 
 
-@bp.route("/grep_processes")
-def grep_processes():
     name = request.args.get("name")
     # vulnerability: Remote Code Execution
     res = subprocess.run(
-        ["ps aux | grep " + name + " | awk '{print $11}'"],
+        ["ps", "aux"],
         shell=True,
         capture_output=True,
     )
